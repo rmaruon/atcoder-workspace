e2be909fba8bb91a90d3689392d4a6e87fa899ff
rmaruon <22119585+rmaruon@users.noreply.github.com>
2020-05-18 21:05:19 +0900

    solve: abc086 A

---
+++ contests/abc086/A/main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import sys
+read = sys.stdin.buffer.read
+readline = sys.stdin.buffer.readline
+readlines = sys.stdin.buffer.readlines
+
+a, b = map(int, read().split())
+
+if a * b % 2 == 0:
+    print('Even')
+else:
+    print('Odd')
