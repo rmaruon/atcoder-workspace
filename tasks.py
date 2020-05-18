2bb8f785b8764cd2fb15adeacb50c5da2edfd78f
rmaruon <22119585+rmaruon@users.noreply.github.com>
2020-05-18 21:03:53 +0900

    chore: update invoke tasks

--- tasks.py
+++ tasks.py
@@ -6,6 +6,9 @@ ROOT = os.path.dirname(__file__)
 
 @invoke.task
 def new(c, contest_id):
+    """
+    コンテスト環境を用意する
+    """
     toml_path = os.path.join(ROOT, 'config', 'atcodertools.toml')
     workspace_path = os.path.join(ROOT, 'contests')
     template_path = os.path.join(ROOT, 'config', 'template.py')
@@ -20,5 +23,19 @@ def new(c, contest_id):
 
 @invoke.task
 def test(c):
-    command = ("atcoder-tools test")
-    invoke.run(command, pty=True)
+    """
+    ローカルテストを実行する
+    """
+    command = "atcoder-tools test"
+    c.run(command, pty=True)
+
+
+@invoke.task(help={'update': '再提出する'})
+def submit(c, update=False):
+    """
+    ソースコードを提出する
+    """
+    command = "atcoder-tools submit"
+    if update:
+        command += " -u"
+    c.run(command, pty=True)
