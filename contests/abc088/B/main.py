81dea747a88dea38bac6c5648ad43da575b72113
rmaruon <22119585+rmaruon@users.noreply.github.com>
2020-05-19 18:10:50 +0900

    solve: abc088 B

---
+++ contests/abc088/B/main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import sys
+read = sys.stdin.buffer.read
+readline = sys.stdin.buffer.readline
+readlines = sys.stdin.buffer.readlines
+
+N = int(readline())
+A = list(map(int, readline().split()))
+A.sort(reverse=True)
+
+alice = bob = 0
+
+for i, num in enumerate(A):
+    if i % 2 == 0:
+        alice += num
+    else:
+        bob += num
+
+print(alice - bob)
