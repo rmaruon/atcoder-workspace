5fc07d800f3e240e28c541b7cff0f524b41ae167
rmaruon <22119585+rmaruon@users.noreply.github.com>
2020-05-19 17:59:24 +0900

    solve: abc083 B

---
+++ contests/abc083/B/main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import sys
+read = sys.stdin.buffer.read
+readline = sys.stdin.buffer.readline
+readlines = sys.stdin.buffer.readlines
+
+N, A, B = map(int, read().split())
+count = 0
+
+for num in range(N + 1):
+    total = sum(map(int, list(str(num))))
+    if A <= total <= B:
+        count += num
+
+print(count)
