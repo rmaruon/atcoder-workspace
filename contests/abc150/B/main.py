01ab400cdfa03737611862b57c685415bcbd689e
rmaruon <22119585+rmaruon@users.noreply.github.com>
2020-05-19 22:36:29 +0900

    solve: abc150 B

---
+++ contests/abc150/B/main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import sys
+read = sys.stdin.buffer.read
+readline = sys.stdin.buffer.readline
+readlines = sys.stdin.buffer.readlines
+
+_ = readline()
+S = input()
+
+print(S.count('ABC'))
