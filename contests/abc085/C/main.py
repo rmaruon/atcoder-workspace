4a4ba1272168f9474aa7722f4911085841d8c79e
rmaruon <22119585+rmaruon@users.noreply.github.com>
2020-05-19 19:21:23 +0900

    solve: abc085 C

---
+++ contests/abc085/C/main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import sys
+read = sys.stdin.buffer.read
+readline = sys.stdin.buffer.readline
+readlines = sys.stdin.buffer.readlines
+
+N, Y = map(int, read().split())
+
+for i in range(N + 1):
+    for j in range(N + 1 - i):
+        if (i * 10000 + j * 5000 + (N - i - j) * 1000) == Y:
+            print(i, j, N - i - j)
+            exit()
+
+print('-1 -1 -1')
