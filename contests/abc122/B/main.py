788928b4b6c8b35b5f341a01538b495e53d835b6
rmaruon <22119585+rmaruon@users.noreply.github.com>
2020-05-20 20:35:34 +0900

    solve: abc122 B

---
+++ contests/abc122/B/main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+S = input()
+
+s = ''
+base = ('A', 'C', 'G', 'T')
+max_len = 0
+
+for c in S:
+    if c not in base:
+        s = ''
+        continue
+
+    s += c
+
+    current_len = len(s)
+    if max_len < current_len:
+        max_len = current_len
+
+print(max_len)
