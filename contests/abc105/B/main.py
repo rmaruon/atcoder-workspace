dd25b41b95b285accea84012b46367d98d445515
rmaruon <22119585+rmaruon@users.noreply.github.com>
2020-05-20 19:40:26 +0900

    solve: abc105 B

---
+++ contests/abc105/B/main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+N = int(input())
+
+for x in range(N // 7 + 1):
+    if (N - 7 * x) % 4 == 0:
+        print('Yes')
+        break
+else:
+    print('No')
+
+# 7x + 4y = N
+# y = (N - 7x) / 4 (4で割り切れる)
