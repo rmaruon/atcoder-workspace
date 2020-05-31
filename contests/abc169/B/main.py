79911d0be4c2a3d9bd28da13b65ed069984b4586
rmaruon <22119585+rmaruon@users.noreply.github.com>
2020-05-31 23:07:32 +0900

    solve: abc169 B

---
+++ contests/abc169/B/main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import sys
+readline = sys.stdin.buffer.readline
+
+N = int(readline())
+A = list(map(int, readline().split()))
+A.sort(reverse=True)
+
+if A[-1] == 0:
+    print(0)
+    exit()
+
+for i, x in enumerate(A):
+    if i == 0:
+        result = x
+        continue
+
+    result = result * x
+
+    if result > 10**18:
+        print(-1)
+        exit()
+
+else:
+    print(result)
