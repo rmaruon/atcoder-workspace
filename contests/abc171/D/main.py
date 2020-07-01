50008bcb0c011cac20a341553cec58160bf1da8e
rmaruon <22119585+rmaruon@users.noreply.github.com>
2020-07-02 02:10:40 +0900

    solve: abc171 D

--- contests/abc171/D/main.py
+++ contests/abc171/D/main.py
@@ -3,30 +3,20 @@ import sys
 from collections import Counter
 readline = sys.stdin.buffer.readline
 
-
-def sum_d(d):
-    total = 0
-    for k, v in d.items():
-        total += k * v
-    return total
-
-
 N = int(readline())
-A = map(int, readline().split())
+A = list(map(int, readline().split()))
 Q = int(readline())
 d = Counter(A)
 
-answer = [0] * Q
+total = sum(A)
 
-for i in range(Q):
+for _ in range(Q):
     B, C = map(int, readline().split())
 
-    if B in d:
-        if C in d:
-            d[C] = d[C] + d.pop(B)
-        else:
-            d[C] = d.pop(B)
+    total -= B * d[B]
+    total += C * d[B]
 
-    answer[i] = sum_d(d)
+    d[C] += d[B]
+    d[B] = 0
 
-print(*answer, sep='\n')
+    print(total)
