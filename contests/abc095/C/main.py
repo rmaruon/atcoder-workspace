e58975dbfe513365ac2a22ab4559a4b32569c8cd
rmaruon <22119585+rmaruon@users.noreply.github.com>
2020-07-02 10:45:04 +0900

    solve: abc095 C

--- contests/abc095/C/main.py
+++ contests/abc095/C/main.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
-import sys
-read = sys.stdin.buffer.read
-readline = sys.stdin.buffer.readline
-readlines = sys.stdin.buffer.readlines
+A, B, C, X, Y = map(int, input().split())
 
-N = map(int, read().split())
+patterns = [0] * 4
+patterns[0] = A * X + B * Y
+patterns[1] = C * max(X, Y) * 2
+patterns[2] = C * Y * 2 + A * (X - Y) if X > Y else float('inf')
+patterns[3] = C * X * 2 + B * (Y - X) if X < Y else float('inf')
+
+print(min(patterns))
