8237dd19b20b10d91cd43d15ff770141f708385a
rmaruon <22119585+rmaruon@users.noreply.github.com>
2020-07-02 11:24:05 +0900

    solve: abc145 C

--- contests/abc145/C/main.py
+++ contests/abc145/C/main.py
@@ -1,7 +1,26 @@
 #!/usr/bin/env python3
+import math
 import sys
-read = sys.stdin.buffer.read
-readline = sys.stdin.buffer.readline
+from itertools import permutations
+
 readlines = sys.stdin.buffer.readlines
 
-N = map(int, read().split())
+N = int(input())
+patterns = [tuple(map(int, input().split())) for _ in range(N)]
+
+total = 0
+n = 0
+
+for pattern in permutations(patterns):
+
+    for i in range(len(pattern) - 1):
+        town_a = pattern[i]
+        town_b = pattern[i + 1]
+
+        distance = math.sqrt((town_a[0] - town_b[0])**2 +
+                             (town_a[1] - town_b[1])**2)
+        total += distance
+
+    n += 1
+
+print(total / n)
