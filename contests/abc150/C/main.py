214cf3a03b7f71eeeebbc77037e5084c3b3cc18d
rmaruon <22119585+rmaruon@users.noreply.github.com>
2020-07-02 12:02:31 +0900

    solve: abc150 C

--- contests/abc150/C/main.py
+++ contests/abc150/C/main.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python3
-import sys
-read = sys.stdin.buffer.read
-readline = sys.stdin.buffer.readline
-readlines = sys.stdin.buffer.readlines
+from itertools import permutations
 
-N = map(int, read().split())
+N = int(input())
+P = tuple(map(int, input().split()))
+Q = tuple(map(int, input().split()))
+
+rank_P = rank_Q = 0
+
+for i, t in enumerate(permutations(range(1, N + 1)), start=1):
+    if t == P:
+        rank_P = i
+        if rank_Q:
+            break
+
+    if t == Q:
+        rank_Q = i
+        if rank_P:
+            break
+
+print(abs(rank_P - rank_Q))
