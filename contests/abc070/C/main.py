eb0fffe9fb0c81fb6cf10f2d9ea9ba5038e8f9ec
rmaruon <22119585+rmaruon@users.noreply.github.com>
2020-07-02 17:12:39 +0900

    solve: abc070 C

--- contests/abc070/C/main.py
+++ contests/abc070/C/main.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
-import sys
-read = sys.stdin.buffer.read
-readline = sys.stdin.buffer.readline
-readlines = sys.stdin.buffer.readlines
+import math
+from functools import reduce
 
-N = map(int, read().split())
+
+def lcm(x, y):
+    return (x * y) // math.gcd(x, y)
+
+
+def lcm_list(numbers):
+    return reduce(lcm, numbers)
+
+
+N = int(input())
+T = [int(input()) for _ in range(N)]
+print(lcm_list(T))
