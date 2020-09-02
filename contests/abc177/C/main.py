d016b76f517c877f88483e3fcff808c22f4d4ae1
rmaruon <22119585+rmaruon@users.noreply.github.com>
2020-09-03 02:47:20 +0900

    solve: abc177 C

---
+++ contests/abc177/C/main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+
+def solve(N, A):
+    MOD = 10**9 + 7
+    total = sum(A)
+    ans = 0
+
+    for i in range(N - 1):
+        total -= A[i]
+        ans += A[i] * total
+        ans %= MOD
+
+    return ans
+
+
+def main():
+    N = int(input())
+    A = list(map(int, input().split()))
+
+    ans = solve(N, A)
+
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()
