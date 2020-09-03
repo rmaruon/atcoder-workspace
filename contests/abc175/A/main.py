c11f7eab10006d0e6913284765032c5482260513
rmaruon <22119585+rmaruon@users.noreply.github.com>
2020-09-03 11:19:42 +0900

    solve: abc175 A

---
+++ contests/abc175/A/main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+S = input()
+count = 0
+max_days = 0
+
+for x in S:
+    if x == 'S':
+        count = 0
+
+    if x == 'R':
+        count += 1
+        if count > max_days:
+            max_days = count
+
+print(max_days)
