c02e9b4dd5502e32ed58b99591ba08424a3cad2b
rmaruon <22119585+rmaruon@users.noreply.github.com>
2021-12-10 12:49:38 +0900

    solve: abc187 A

--- contests/abc187/A/main.py
+++ contests/abc187/A/main.py
@@ -1,8 +1,21 @@
 #!/usr/bin/env python3
-import sys
 
-read = sys.stdin.buffer.read
-readline = sys.stdin.buffer.readline
-readlines = sys.stdin.buffer.readlines
 
-N = map(int, read().split())
+def S(n):
+    a, b, c = map(int, list(str(n)))
+    return a + b + c
+
+
+def main():
+    A, B = map(int, input().split())
+    sa = S(A)
+    sb = S(B)
+
+    if sa >= sb:
+        print(sa)
+    else:
+        print(sb)
+
+
+if __name__ == "__main__":
+    main()
