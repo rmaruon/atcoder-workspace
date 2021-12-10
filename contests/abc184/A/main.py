ef09315c67e5837dda006f443aad9287b6727823
rmaruon <22119585+rmaruon@users.noreply.github.com>
2021-12-10 12:35:53 +0900

    solve: abc184 A

--- contests/abc184/A/main.py
+++ contests/abc184/A/main.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
-import sys
 
-read = sys.stdin.buffer.read
-readline = sys.stdin.buffer.readline
-readlines = sys.stdin.buffer.readlines
+a, b = map(int, input().split())
+c, d = map(int, input().split())
 
-N = map(int, read().split())
+print(a * d - b * c)
