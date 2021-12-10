8ef366e6612dfd6abef502b7b95b33c588645745
rmaruon <22119585+rmaruon@users.noreply.github.com>
2021-12-10 12:42:01 +0900

    solve: abc186 A

--- contests/abc186/A/main.py
+++ contests/abc186/A/main.py
@@ -1,8 +1,4 @@
 #!/usr/bin/env python3
-import sys
 
-read = sys.stdin.buffer.read
-readline = sys.stdin.buffer.readline
-readlines = sys.stdin.buffer.readlines
-
-N = map(int, read().split())
+N, W = map(int, input().split())
+print(int(N / W))
