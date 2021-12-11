010de611c02ed18f0930e84483be9e4fe19dc253
rmaruon <22119585+rmaruon@users.noreply.github.com>
2021-12-12 00:47:01 +0900

    solve: abc192 A

--- contests/abc192/A/main.py
+++ contests/abc192/A/main.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
-import sys
 
-read = sys.stdin.buffer.read
-readline = sys.stdin.buffer.readline
-readlines = sys.stdin.buffer.readlines
-
-N = map(int, read().split())
+X = int(input())
+current = X % 100
+next = 100 - current
+print(next)
