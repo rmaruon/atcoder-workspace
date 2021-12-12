813c77333b6849ef4780d0cffde5f5b118f7b24d
rmaruon <22119585+rmaruon@users.noreply.github.com>
2021-12-12 18:34:54 +0900

    solve: abc194 A

--- contests/abc194/A/main.py
+++ contests/abc194/A/main.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
-import sys
 
-read = sys.stdin.buffer.read
-readline = sys.stdin.buffer.readline
-readlines = sys.stdin.buffer.readlines
+A, B = [int(x) for x in input().split()]
 
-N = map(int, read().split())
+無脂乳固形分 = A
+乳脂肪分 = B
+乳固形分 = 無脂乳固形分 + 乳脂肪分
+
+if 乳固形分 >= 15 and 乳脂肪分 >= 8:
+    print(1)
+elif 乳固形分 >= 10 and 乳脂肪分 >= 3:
+    print(2)
+elif 乳固形分 >= 3:
+    print(3)
+else:
+    print(4)
