02f05e5cc382110a5c97c93b93152b46bd186f12
rmaruon <22119585+rmaruon@users.noreply.github.com>
2021-12-21 09:54:24 +0900

    update abc226 A

--- contests/abc226/A/main.py
+++ contests/abc226/A/main.py
@@ -3,12 +3,8 @@ import sys
 
 
 def solve(X: float):
-    a, b = str(X).split(".")
-
-    if int(b[0]) >= 5:
-        print(int(a) + 1)
-    else:
-        print(a)
+    result = int(round(X + 0.0001, 0))
+    print(result)
 
 
 def main():
